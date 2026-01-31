ec45c9f7e61835f43d662923a8f3da8291c7957f
Abhishek-00000 <abhishekchauhan78763@gmail.com>
2026-01-31 18:31:43 +0530

    Backend: Setup Database

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () =>  {
+    console.log(`Listening on port ${port}`);
+})
